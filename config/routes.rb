d6480ddd21c861659cee180bb085e110338dae35
Julien Schmit <julien.schmit@ehl.ch>
2017-11-14 15:52:57 +0100

    initialliated the controller Article

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'article/index'
+
+  get 'article/show'
+
+  get 'article/new'
+
+  get 'article/create'
+
+  get 'article/edit'
+
+  get 'article/update'
+
+  get 'article/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
